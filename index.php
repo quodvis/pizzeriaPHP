c0c64bf125f20f7d42cca1ce186df8a12b2977b7
Reinaldo Orrego <reinaldo.orrego@uchile.cl>
2020-06-03 23:16:28 -0400

    se crea formulario

--- index.php
+++ index.php
@@ -4,6 +4,18 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+<?php
+    error_reporting(0);
+    if ($_REQUEST['clear']){
+        $val_NAME="";
+        $val_ADDRESS="";
+        $result="";
+    }
+    if ($_REQUEST['submit']){
+        $NOMBRE = $_REQUEST['val_NAME'];
+        $DIRECCION = $_REQUEST['val_ADDRESS'];
+    }
+?>
 <html>
     <head>
         <meta charset="UTF-8">
@@ -13,7 +25,18 @@ and open the template in the editor.
         
         <header><img src="images/pizza5400x2342.jpg" width=100%>
        </header>
-        
+        <h1>Pizzeria los Inmigrantes</h1>
+        <p>
+        <form name="form" method="past" action="">
+            <p>Ingrese su nombre <input type="text" name="val_NAME" value="<?php echo $val_NAME; ?>"></p>
+            <p>Ingrese su dirección <input type="text" name="val_ADDRESS" value="<?PHP echo $val_ADDRESS; ?>"></p>
+            
+            <input type="submit" name="submit" value="Ingresar">
+            <input type="submit" name="clear" value="Clear">
+        </form>
+            
+            
+        </p>
 
     </body>
 </html>
