be88e78e33c08e02290baf28a09b6ea4fd96cdb8
Reinaldo Orrego <reinaldo.orrego@uchile.cl>
2020-06-06 21:40:40 -0400

    se modifica pagina principal y se agrega css

--- index.php
+++ index.php
@@ -17,26 +17,73 @@ and open the template in the editor.
     }
 ?>
 <html>
+    
     <head>
         <meta charset="UTF-8">
+        <meta http-equiv="refresh" content="1" \>
+        <link rel=stylesheet href="style.css">
+        <link href="https://fonts.googleapis.com/css2?family=Sawarabi+Mincho&display=swap" rel="stylesheet">
+
         <title>Pizzería los inmigrantes</title>
+        
     </head>
     <body>
         
-        <header><img src="images/pizza5400x2342.jpg" width=100%>
-       </header>
+        <header><img src="images/pizza1000x200.jpg" width=100%></header>
         <h1>Pizzeria los Inmigrantes</h1>
-        <p>
-        <form name="form" method="past" action="">
-            <p>Ingrese su nombre <input type="text" name="val_NAME" value="<?php echo $val_NAME; ?>"></p>
-            <p>Ingrese su dirección <input type="text" name="val_ADDRESS" value="<?PHP echo $val_ADDRESS; ?>"></p>
-            
-            <input type="submit" name="submit" value="Ingresar">
-            <input type="submit" name="clear" value="Clear">
-        </form>
-            
-            
-        </p>
-
+        <h2>Registrarse</h2>
+        <table border="0">
+            <form name="form" method="past" action="">
+                <tr>
+                    <td>
+                        Ingrese su nombre
+                    </td>
+                    <td>
+                        <input type="text" name="val_NAME" value="<?php echo $val_NAME; ?>">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Ingrese su dirección
+                    </td>
+                    <td>
+                        <input type="text" name="val_ADDRESS" value="<?PHP echo $val_ADDRESS; ?>">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Ingrese su rut
+                    </td>
+                    <td>
+                        <input type="text" name="val_RUT" value="<?PHP echo $val_RUT; ?>">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Ingrese su contraseña
+                    </td>
+                    <td>
+                        <input type="text" name="val_PASS" value="<?PHP echo $val_PASS; ?>">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Ingrese su contraseña nuevamente
+                    </td>
+                    <td>
+                        <input type="text" name="val_PASS2" value="<?PHP echo $val_PASS; ?>">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                    </td>
+                    <td>
+                        <input type="submit" name="submit" value="Ingresar">
+                        <input type="submit" name="clear" value="Clear">
+                        
+                    </td>
+                </tr>
+            </form>
+        </table>
     </body>
 </html>
